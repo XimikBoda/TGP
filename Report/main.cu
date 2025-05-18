7ff98220e8cb5a76167611daaf36a9dea12aa532
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2025-05-18 21:43:16 +0300

    Added base sfml and imgui

---
+++ Report/main.cu
@@ -0,0 +1,166 @@
+﻿#include <iostream>
+#include <cmath>
+#include <thread>
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <inttypes.h>
+
+
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+__global__
+void initWith(float num, float* a, int N)
+{
+
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
+	int stride = blockDim.x * gridDim.x;
+
+	for (int i = index; i < N; i += stride)
+	{
+		a[i] = num;
+	}
+}
+
+__global__
+void addVectorsInto(float* result, float* a, float* b, int N)
+{
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
+	int stride = blockDim.x * gridDim.x;
+
+	for (int i = index; i < N; i += stride)
+	{
+		result[i] = a[i] + b[i];
+	}
+}
+
+void checkElementsAre(float target, float* vector, int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		if (vector[i] != target)
+		{
+			printf("FAIL: vector[%d] - %0.0f does not equal %0.0f\n", i, vector[i], target);
+			exit(1);
+		}
+	}
+	printf("Success! All values calculated correctly.\n");
+}
+
+int main2()
+{
+	int deviceId;
+	int numberOfSMs;
+
+	cudaGetDevice(&deviceId);
+	cudaDeviceGetAttribute(&numberOfSMs, cudaDevAttrMultiProcessorCount, deviceId);
+
+	const int N = 2 << 24;
+	size_t size = N * sizeof(float);
+
+	//float *a;
+	//float *b;
+	float* c;
+	// Punteros que apuntarán a posiciones de memoria con los que trabajará la CPU
+
+	//a = (float *) malloc(size);
+	//b = (float *) malloc(size);
+	c = (float*)malloc(size);
+	// Asignamos espacio en la memoria que trabaja la CPU
+
+	float* da;
+	float* db;
+	float* dc;
+	// Punteros que apuntan a posiciones de la memoria de vídeo con los que trabajará la GPU
+
+	cudaMalloc(&da, size);
+	cudaMalloc(&db, size);
+	cudaMalloc(&dc, size);
+	// Asignamos espacio en la memoria de vídeo de la GPU
+
+	size_t threadsPerBlock;
+	size_t numberOfBlocks;
+
+	threadsPerBlock = 256;
+	numberOfBlocks = 32 * numberOfSMs;
+
+	cudaError_t addVectorsErr;
+	cudaError_t asyncErr;
+
+	initWith << <numberOfBlocks, threadsPerBlock >> > (3, da, N);
+	initWith << <numberOfBlocks, threadsPerBlock >> > (4, db, N);
+	initWith << <numberOfBlocks, threadsPerBlock >> > (0, dc, N);
+
+	addVectorsInto << <numberOfBlocks, threadsPerBlock >> > (dc, da, db, N);
+	// Les pasamos los punteros de device (da,db,dc) y no de host (a,b,c)
+
+	addVectorsErr = cudaGetLastError();
+	if (addVectorsErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(addVectorsErr));
+
+	asyncErr = cudaDeviceSynchronize();
+	if (asyncErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(asyncErr));
+
+	//cudaMemcpy(a, da, size, cudaMemcpyDeviceToHost);
+	//cudaMemcpy(b, db, size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(c, dc, size, cudaMemcpyDeviceToHost);
+	// Copiamos los resultados (del último vector, el resultante) de la GPU a la CPU
+
+	checkElementsAre(7, c, N);
+
+	cudaFree(da);
+	cudaFree(db);
+	cudaFree(dc);
+	// Liberamos la memoria de vídeo
+
+	//free(a);
+	//free(b);
+	free(c);
+	// Liberamos la memoria principal que trabajaba la CPU
+
+	return 0;
+}
+
+
+
+int main() {
+	main2();
+
+	sf::RenderWindow window(sf::VideoMode(1600, 900), "Test Place");
+	window.setFramerateLimit(60);
+	ImGui::SFML::Init(window);
+
+	sf::CircleShape point(2.f);
+	point.setFillColor(sf::Color::Green);
+	point.setOrigin(2.f,2.f);
+
+	sf::Clock deltaClock;
+	sf::Clock Clock;
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::Resized:
+				window.setView(sf::View(sf::FloatRect(0.f, 0.f, (float)event.size.width, (float)event.size.height)));
+				break;
+			}
+		}
+
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::SFML::Render(window);
+		window.display();
+		window.clear();
+	}
+
+	ImGui::SFML::Shutdown();
+}
